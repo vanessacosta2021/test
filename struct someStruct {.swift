4faa776bae58060e5641013a8660cdb4b52a6c73
Ivan Cantarino <ivan.cantarino@icloud.com>
2021-08-26 20:23:14 +0100

    c1

--- struct someStruct {.swift
+++ struct someStruct {.swift
@@ -1,11 +1,9 @@
 struct someStruct {
     let a: String
     let b: String
-<<<<<<< HEAD
     let b1: String
-=======
     let a1: String
->>>>>>> 8a2a2d32c65810774ad5c634a20ac268106f08b2
+    let c1: String
 }
 
 struct anotherStruct {
